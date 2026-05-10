13adfea88757935b12523a3c23f0ff9b15104873
jiaqi-ii <jqwong007@mymail.sim.edu.sg>
2026-05-11 02:47:33 +0800

    updated reports

--- models/Favourite.js
+++
@@ -1,9 +0,0 @@
-class Favourite {
-  constructor(id, user_id, fundraiser_id) {
-    this.id = id;
-    this.user_id = user_id;
-    this.fundraiser_id = fundraiser_id;
-  }
-}
-
-module.exports = Favourite;
