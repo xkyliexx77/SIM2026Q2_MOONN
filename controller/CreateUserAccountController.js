b19eaab5420c4acc24dece644465c5618e4eb013
jiaqi-ii <jqwong007@mymail.sim.edu.sg>
2026-05-13 19:19:10 +0800

    update login.html

--- controller/CreateUserAccountController.js
+++ controller/CreateUserAccountController.js
@@ -1,9 +1,24 @@
-const UserAccountEntity = require('../entity/UserAccountEntity');
+const bcrypt = require('bcrypt');
+
+const UserAccountEntity =
+  require('../entity/UserAccountEntity');
 
 class CreateUserAccountController {
+
   static async create(data) {
-    return await UserAccountEntity.create(data);
+
+    const hashedPassword =
+      await bcrypt.hash(data.password, 10);
+
+    return await UserAccountEntity.create({
+      name: data.name,
+      email: data.email,
+      password: hashedPassword,
+      role: data.role
+    });
+
   }
+
 }
 
 module.exports = CreateUserAccountController;
